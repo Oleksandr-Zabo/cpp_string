5d5007acf1fe5f6c89bc14e9fab75cb4c0624bb0
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 17:35:16 +0300

    ex. 2 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void del_index(char* str, char elem) {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] == elem) {
+            for (int j = i; j < len; j++) {
+                str[j] = str[j + 1];
+            }
+            len--; 
+            i--;  
+        }
+    }
+}
+
+int main() {
+    char str[] = "hello  world!!! ";
+    cout << "Before: " << str << endl;
+
+    char symbol;
+    cout << "Enter symbol: ";
+    cin >> symbol;
+
+    del_index(str, symbol);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
