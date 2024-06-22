90972d2dee4fbd5b061ec54dcbbd6383e5759654
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 16:44:51 +0300

    ex. 2 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void num_elem(char* str, int num = 0, int letter = 0, int other = 0) 
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >=48 && str[i]<=57) {
+            num ++;
+        }
+        else if (str[i] >= 65 && str[i] <= 90 || str[i] >= 97 && str[i] <= 122) {
+            letter++;
+        }
+        else {
+            other++;
+        }
+    }
+
+    cout << "Numbers: " << num << endl;
+    cout << "Letters: " << letter << endl;
+    cout << "Other char: " << other << endl;
+}
+
+int main() {
+
+    char str[] = "hello 2+4 => 6";
+
+    cout << "Before: " << str << endl;
+
+    num_elem(str);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
