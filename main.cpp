1e5a8e720a1b0f117c987a8f30aeb2047ef7c559
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 18:08:48 +0300

    ex.6 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void num_elem(char* str, int num = 0, int letter = 0, int other = 0)
+{
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 48 && str[i] <= 57) {
+            num++;
+        }
+        else if (str[i] >= 65 && str[i] <= 90 || str[i] >= 97 && str[i] <= 122) {
+            letter++;
+        }
+        else {
+            other++;
+        }
+    }
+
+    cout << "Numbers: " << num << endl;
+    cout << "Letters: " << letter << endl;
+    cout << "Other char: " << other << endl;
+}
+
+int main() {
+    const int MAX_SIZE = 100;
+
+    char str[MAX_SIZE];
+    cout << "Enter string: ";
+    cin.getline(str, MAX_SIZE);
+
+    num_elem(str);
+
+    return 0;
 }
