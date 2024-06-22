f6ba59391f393a90491a5426ad3b7b952acacebf
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 17:59:36 +0300

    ex. 4 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void dot_to_exp(char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '.') {
+            str[i] = '!';
+
+        }
+    }
+}
+
+int main() {
+    const int MAX_SIZE = 100;
+    char str[MAX_SIZE];
+    cout << "Enter string: ";
+    cin.getline(str, MAX_SIZE);// str with spaces
+    cout << "Before: " << str << endl;
+
+    dot_to_exp(str);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
