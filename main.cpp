627d8e9157adf533bc5295572161238905bdf8d7
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 16:34:27 +0300

    ex. 1 started

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void space_to_tab(char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '\t';
+            i += 2;
+            
+        }
+    }
+}
+
+int main() {
+    char str[] = "hello world  mj j jjj h ghgg";
+    cout << "Before: " << str << endl;
+
+    space_to_tab(str);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
