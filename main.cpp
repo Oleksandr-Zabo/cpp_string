129a8ea1ca80fd9da67e31a84d2ed0ecf7d935cf
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 17:20:58 +0300

    ex. 1 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void del_index(char* str, int index) {
+    for (int i = index; i < strlen(str); i++) {
+        str[i] = str[i + 1];
+    }
+}
+
+int main() {
+    char str[] = "hello world !!!";
+    cout << "Before: " << str << endl;
+
+    int index;
+    do {
+        cout << "Enter index: ";
+        cin >> index;
+    } while (index < 0 || index >= strlen(str));
+
+    del_index(str, index);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
