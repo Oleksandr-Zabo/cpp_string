90a94da083ed7e942e85c9ca029597a7fc00d0f2
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 17:48:34 +0300

    ex. 3 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+void insert(char* str, char elem, int index) {
+    int len = strlen(str);
+    for (int i = len; i > index; i--) {
+        
+        str[i] = str[i - 1];
+    }
+    len++;
+    str[index] = elem;
+    str[len - 1] = '\0';
+}
+
+int main() {
+    char str[] = "hello  world!!! ";
+    cout << "Before: " << str << endl;
+
+    char symbol;
+    cout << "Enter symbol: ";
+    cin >> symbol;
+
+    int index;
+    do
+    {
+        cout << "Enter index: ";
+        cin >> index;
+    } while (index<0 || index>strlen(str));
+    
+
+    insert(str, symbol, index);
+
+    cout << "After: " << str << endl;
+
+    return 0;
 }
