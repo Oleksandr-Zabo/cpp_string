cbe2a49fdba78391a2ca771522cf1c52be089094
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-22 18:04:36 +0300

    ex. 5 hw hsd done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{ 
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+int find_elem(char* str, char symbol, int count_el = 0) {
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == symbol) {
+            count_el++;
+
+        }
+    }
+    return count_el;
+}
+
+int main() {
+    const int MAX_SIZE = 100;
+    char str[MAX_SIZE];
+    cout << "Enter string: ";
+    cin.getline(str, MAX_SIZE);// str with spaces
+
+    
+    char symbol;
+    cout << "Enter symbol: ";
+    cin >> symbol;
+
+    int count = find_elem(str, symbol);
+
+    cout << "Number of symbol: " << count << endl;
+
+    return 0;
 }
